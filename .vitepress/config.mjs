0a041591bb40acc734f68ef21e798c5a34f84134
Rex-Seraphim <93363203+Rex-Seraphim@users.noreply.github.com>
2024-12-23 20:59:59 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,71 @@
+import { defineConfig } from 'vitepress'
+//import { set_siderbar} from "../utils/auto-sidebar.js"; //自动生成左侧边栏
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  head:[["link",{rel: "icon", href: "/logo.png"}]],
+
+  title: "Personal-Diary",
+  description: "A VitePress Site",
+  themeConfig: {
+    logo:'/logo.png',
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [  
+
+      // 这个目录下可以有多个章节，text是一个小章节的标题，其中可以包括多个内容
+
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown 演示', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      },
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown 演示', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+      
+    ],
+
+    // sidebar: { "/front-end/react": set_sidebar("front-end/react") },
+    //这个好像有自动化，但是貌似教程写的比较细碎，再说吧
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/Rex-Seraphim' }
+    ],
+    footer:{
+      copyright:"Copyright @ 2024 personal-diary RexSeraphim"
+    },
+   // 设置搜索框的样式
+   search: {
+    provider: "local",
+    options: {
+      translations: {
+        button: {
+          buttonText: "搜索文档",
+          buttonAriaLabel: "搜索文档",
+        },
+        modal: {
+          noResultsText: "无法找到相关结果",
+          resetButtonTitle: "清除查询条件",
+          footer: {
+            selectText: "选择",
+            navigateText: "切换",
+          },
+        },
+      },
+    },
+  },
+
+
+  }
+})
---
+++ index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "RexSeraphim"
+  text: "Personal-Diary"
+  tagline: Make Diary Greate Again
+  image:
+    src: /云服务器,云上传.png
+    alt: 背景图片
+  actions:
+    - theme: brand
+      text: Markdown 示例
+      link: /markdown-examples
+    - theme: alt
+      text: API 示例
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: A的详细描述
+  - title: Feature B
+    details: B的详细描述
+  - title: Feature C
+    details: C的详细描述
+---
+
