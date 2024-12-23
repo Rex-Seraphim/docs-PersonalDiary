60fdd9fd482c018f5bd8b9a20134404bf3d55f1b
Rex-Seraphim <93363203+Rex-Seraphim@users.noreply.github.com>
2024-12-23 22:30:55 +0800

    注释base

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: "/docs-demo/",
+  // base: "/docs-demo/",
 
   head:[["link",{rel: "icon", href: "/docs-demo/logo.png"}]],
 
